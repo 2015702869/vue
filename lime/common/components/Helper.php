088e80d447e814129e88ff21a5cd2e3ef391958b
wangzijian <your@qq.com>
2022-05-16 14:43:40 +0800

    lime 5/16

--- lime/common/components/Helper.php
+++ lime/common/components/Helper.php
@@ -2,10 +2,21 @@
 namespace app\common\components;
 class Helper
 {
+	/**
+	 * [encryption description]
+	 * @param  [str] $password [密码]
+	 * @return [str]           [加密后密码]
+	 */
     public function encryption ($password)
     {
         return $password = password_hash($password,PASSWORD_DEFAULT);
     }
+    /**
+     * [decryption description]
+     * @param  [str] $password [密码]
+     * @param  [str] $hash     [解密密码]
+     * @return [bool]           [真假状态]
+     */
     public function decryption ($password,$hash)
     {
 		if (password_verify($password, $hash)) {
--- lime/modules/admin/controllers/LoginController.php
+++ lime/modules/admin/controllers/LoginController.php
@@ -9,9 +9,10 @@ use app\modules\admin\models\Admin;
 
 class LoginController extends Controller
 {
-	public $enableCsrfValidation = false;
+	public $enableCsrfValidation = false;//关闭csrf表单验证
 	public function behaviors()
 	{
+		//设置请求头
 	    return [
 	        'corsFilter'=>[
 	            'class' => Cors::className(),
@@ -24,8 +25,9 @@ class LoginController extends Controller
 	}
 	public function init(){
       	parent::init();	
-		\Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+		\Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;//设置返回类型
     }
+
 	public function actionLogin()
 	{
 		$request = \Yii::$app->request;
@@ -33,23 +35,41 @@ class LoginController extends Controller
 		{
 			$model = new Login;
 			$model->attributes = $request->post();
+			//表单验证
 			if ($model->validate() && !empty(($request->post()))) {
 			    $admin = new Admin();
 			    $password = $request->post('password');
 			    $username = $request->post('username');
 			    $data = $admin->find()->where(['username'=>$username])->one();
-
-			    \Yii::$app->response->data = [
-					"data" => \Yii::$app->helper->decryption($password,$data['password'])
-				];
+			    if($data == null)//验证账号是否正确
+			    {
+			 		return \Yii::$app->response->data = [
+						"data" => false
+					];
+			    }
+			    //调用方法验证用户名密码是否正确
+			    if(\Yii::$app->helper->decryption($password,$data['password']) === true)
+			    {
+			    	$session = \Yii::$app->session;
+			    	$session->set('username',$data['username']);
+			    	$session->set('nickname',$data['nickname']);
+			    	return \Yii::$app->response->data = [
+						"data" => true
+					];
+			    }else{
+			    	return \Yii::$app->response->data = [
+			    		"data" => false
+			    	];
+			    }
 			} else {
+				//验证失败返回
 				$errors = $model->errors;
 				\Yii::$app->response->data = [
 					"data" => $errors
 				];
 			}
 		}else{
-			throw new \yii\web\UnauthorizedHttpException;
+			throw new \yii\web\UnauthorizedHttpException;//请求方法错误返回错误
 		}
 	}
 	public function actionAdd()
